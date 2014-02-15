7d0343d40a5a34a3e7c54610920db2990f538dc5
tgm-patrick-koc <cash1v@hotmail.com>
2014-02-15 16:28:44 +0100

    Gui v2

--- src/geyerhamplkocsis/Gui.java
+++ src/geyerhamplkocsis/Gui.java
@@ -1,20 +1,26 @@
 package geyerhamplkocsis;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class Gui extends JFrame {
-	JPanel eingabe = new JPanel();
-	JPanel buttons = new JPanel();
-	JPanel combobox = new JPanel();
+	JPanel input_panel = new JPanel();
+	JPanel buttons_panel = new JPanel();
+	JPanel combobox_panel = new JPanel();
 	JButton encrypt = new JButton("Encrypt");
 	JButton decrypt = new JButton("Decrypt");
-	JComboBox encryptmethode = new JComboBox();
-	
+	JComboBox<String> encryptmode = new JComboBox<String>();
+	String label_shiftci = "ShiftCipher";
+	String label_keywoci = "KeyWordCipher";
+	String label_substci = "SubstituationCipher";
+	String label_monoalci = "MonoAlphabeticCipher";
+	JTextArea input = new JTextArea("Input");
 	public Gui(){
 		this.setTitle("EncryptDecrypt");
 		this.setVisible(true);
@@ -22,14 +28,22 @@ public class Gui extends JFrame {
 		this.setLayout(new BorderLayout());
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		eingabe.setLayout(new BorderLayout());
-		buttons.setLayout(new BorderLayout());
-		combobox.setLayout(new BorderLayout());
+		buttons_panel.setLayout(new GridLayout(2,1));
 		
 		
-		this.add(eingabe,BorderLayout.CENTER);
-		this.add(buttons,BorderLayout.SOUTH);
-		this.add(combobox,BorderLayout.WEST);
 		
+		encryptmode.addItem(label_shiftci);
+		encryptmode.addItem(label_keywoci);
+		encryptmode.addItem(label_substci);
+		encryptmode.addItem(label_monoalci);
+		combobox_panel.add(encryptmode);
+		buttons_panel.add(encrypt);
+		buttons_panel.add(decrypt);
+		input_panel.add(input);
+		
+		
+		this.add(input_panel,BorderLayout.CENTER);
+		this.add(buttons_panel,BorderLayout.SOUTH);
+		this.add(combobox_panel,BorderLayout.NORTH);
 	}
 }
