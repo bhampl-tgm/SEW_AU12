7151453118973ce399339144f212c431b271c758
Burkhard Hampl <bhampl@student.tgm.ac.at>
2014-02-16 15:52:40 +0100

    Add setter for normalAlphabet

--- src/geyerhamplkocsis/MonoalphabeticCipher.java
+++ src/geyerhamplkocsis/MonoalphabeticCipher.java
@@ -2,40 +2,53 @@ package geyerhamplkocsis;
 
 /**
  * The MonoalphabeticCipher class to decrypt and encrypt a text
+ * 
  * @author Burkhard Hampl
- *
+ * 
  */
 public class MonoalphabeticCipher implements Cipher {
- 
+
 	private String secretAlphabet;
 
 	private final String normalAlphabet = "abcdefghijklmnopqrstuvwxyzäöüß";
-	 
+
 	/**
 	 * The default constructor of the MonoalphabeticCipher class
 	 */
 	public MonoalphabeticCipher() {
 		this.secretAlphabet = this.normalAlphabet;
 	}
-	 
+
 	/**
 	 * The getter method to get secretAlphabet
+	 * 
 	 * @return the secretAlphabet
 	 */
 	public String getSecretAlphabet() {
 		return this.secretAlphabet;
 	}
-	
+
 	/**
 	 * The setter method to set get secretAlphabet
-	 * @param secretAlphabet the secretAlphabet to set
+	 * 
+	 * @param secretAlphabet
+	 *            the secretAlphabet to set
 	 */
 	public void setSecretAlphabet(String secretAlphabet) {
 		this.secretAlphabet = secretAlphabet;
 	}
-	 
+
+	/**
+	 * The setter method to set get normalAlphabet
+	 * @return the normalAlphabet
+	 */
+	public String getNormalAlphabet() {
+		return normalAlphabet;
+	}
+
 	/**
 	 * To encrypt a text
+	 * 
 	 * @return the encrypted text
 	 * @see geyerhamplkocsis.Cipher#encrypt(java.lang.String)
 	 */
@@ -50,12 +63,13 @@ public class MonoalphabeticCipher implements Cipher {
 				encryptedText += this.secretAlphabet.charAt(charPos);
 			}
 		}
-		
+
 		return encryptedText;
 	}
-	 
+
 	/**
 	 * To decrypt a text
+	 * 
 	 * @return the decrypted text
 	 * @see geyerhamplkocsis.Cipher#decrypt(java.lang.String)
 	 */
@@ -69,9 +83,8 @@ public class MonoalphabeticCipher implements Cipher {
 				decryptedText += this.normalAlphabet.charAt(charPos);
 			}
 		}
-		
+
 		return decryptedText;
 	}
-	 
+
 }
- 
