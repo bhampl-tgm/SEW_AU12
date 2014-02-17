dd9aaf1ba2fccad9d2f44bcdbb6b21101932533a
sgeyer <exsoloscripter@gmail.com>
2014-02-17 16:12:12 +0100

    Added KeywordCipher Added ShiftCipher

--- src/geyerhamplkocsis/KeywordCipher.java
+++ src/geyerhamplkocsis/KeywordCipher.java
@@ -1,14 +1,99 @@
 package geyerhamplkocsis;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * To encrypt a String, this class will add a keyword in the beginning of the
+ * secret alphabet. If the Keyword contains Characters twice or illegal
+ * Characters were used, an {@link IllegalArgumentException} will be thrown.
+ * The case of the Characters will be ignored.
+ * 
+ * @author Stefan Geyer
+ * @version 1.0
+ * @since 2014-02-15
+ */
 public class KeywordCipher extends MonoalphabeticCipher {
- 
+
+	/**
+	 * Generates a secret alphabet with the keyword in the beginning. Sets the
+	 * secret alphabet in the {@link MonoalphabeticCipher} class.
+	 * 
+	 * @param keyword
+	 *            Keyword that will be added to the alphabet
+	 */
 	public KeywordCipher(String keyword) {
-	 
+		// Ignoring Upper Case
+		String kw = keyword.toLowerCase();
+
+		List<Character> tmp = new ArrayList<Character>();
+
+		// Adding every char of the keyword to the list
+		for (int i = 0; i < kw.length(); i++) {
+			tmp.add(kw.toCharArray()[i]);
+		}
+
+		for (char c : this.getNormalAlphabet().toCharArray()) {
+			// If the String doesn't contain the char, it will be added to the
+			// list
+			if (convert(tmp).indexOf(c) == -1) {
+				tmp.add(c);
+			}
+		}
+
+		// Sets the secret alphabet by calling the method from the super class
+		this.setSecretAlphabet(convert(tmp));
 	}
-	 
+
+	/**
+	 * Generates a secret alphabet with the keyword in the beginning. Sets the
+	 * secret alphabet in the {@link MonoalphabeticCipher} class.
+	 * 
+	 * @param keyword
+	 *            Keyword that will be added to the alphabet
+	 */
 	public void setKeyword(String keyword) {
-	 
+		// Ignoring Upper Case
+		String kw = keyword.toLowerCase();
+
+		List<Character> tmp = new ArrayList<Character>();
+
+		// Adding every char of the keyword to the list
+		for (int i = 0; i < kw.length(); i++) {
+			tmp.add(kw.toCharArray()[i]);
+		}
+
+		for (char c : this.getNormalAlphabet().toCharArray()) {
+			// If the String doesn't contain the char, it will be added to the
+			// list
+			if (convert(tmp).indexOf(c) == -1) {
+				tmp.add(c);
+			}
+		}
+
+		// Sets the secret alphabet by calling the method from the super class
+		this.setSecretAlphabet(convert(tmp));
 	}
-	 
+
+	/**
+	 * 
+	 * @param al The List that contains the Characters that will be merged together
+	 * @return The merged Characters
+	 */
+	private String convert(List<Character> al) {
+		StringBuilder sb = new StringBuilder();
+
+		//Append every Character of the List to the StringBuilder
+		for (char c : al) {
+			sb.append(c);
+		}
+
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		KeywordCipher kc = new KeywordCipher("hgfawjköä");
+		System.out.println(kc.encrypt("hallowelt"));
+		System.out.println(kc.decrypt(kc.encrypt("hallowelt")));
+	}
 }
- 
--- src/geyerhamplkocsis/ShiftCipher.java
+++ src/geyerhamplkocsis/ShiftCipher.java
@@ -1,14 +1,86 @@
 package geyerhamplkocsis;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * To encrypt a String the last digits of the standard alphabet will be moved.
+ * Example: 
+ * Default Alphabet: abcdefghijklmnopqrstuvwxyz 
+ * Value: 6 Secret
+ * Alphabet: uvwxyzabcdefghijklmnopqrst
+ * 
+ * @author Stefan Geyer
+ * @version 1.0
+ * @since 2014-02-17
+ */
 public class ShiftCipher extends MonoalphabeticCipher {
- 
+
+	/**
+	 * @param value
+	 *            How many digits the default alphabet will be moved.
+	 */
 	public ShiftCipher(int value) {
-	 
+		// If the value is greater than 30 it will be set to modulo value
+		value = value % 30;
+
+		List<Character> tmp = new ArrayList<Character>();
+
+		// Add everything from the end to the value
+		for (int i = (this.getNormalAlphabet().length() - value); i < this
+				.getNormalAlphabet().length(); i++) {
+			tmp.add(this.getNormalAlphabet().toCharArray()[i]);
+		}
+
+		// Add everything from the beginning to the value
+		for (int i = 0; i < (this.getNormalAlphabet().length() - value); i++) {
+			tmp.add(this.getNormalAlphabet().toCharArray()[i]);
+		}
+
+		// Set the secret alphabet by using the method from the Superclass
+		this.setSecretAlphabet(convert(tmp));
 	}
-	 
-	public void setShiftAmount(int shiftvalue) {
-	 
+
+	/**
+	 * Convert the content of a {@link List} to a {@link String}
+	 * 
+	 * @param al
+	 *            The List to convert
+	 * @return The converted {@link String}
+	 */
+	private String convert(List<Character> al) {
+		StringBuilder sb = new StringBuilder();
+
+		// Append every Character of the List to the StringBuilder
+		for (char c : al) {
+			sb.append(c);
+		}
+
+		return sb.toString();
 	}
-	 
+
+	/**
+	 * @param value
+	 *            How many digits the default alphabet will be moved.
+	 */
+	public void setShiftAmount(int value) {
+		// If the value is greater than 30 it will be set to modulo value
+		value = value % 30;
+
+		List<Character> tmp = new ArrayList<Character>();
+
+		// Add everything from the end to the value
+		for (int i = (this.getNormalAlphabet().length() - value); i < this
+				.getNormalAlphabet().length(); i++) {
+			tmp.add(this.getNormalAlphabet().toCharArray()[i]);
+		}
+
+		// Add everything from the beginning to the value
+		for (int i = 0; i < (this.getNormalAlphabet().length() - value); i++) {
+			tmp.add(this.getNormalAlphabet().toCharArray()[i]);
+		}
+
+		// Set the secret alphabet by using the method from the Superclass
+		this.setSecretAlphabet(convert(tmp));
+	}
 }
- 
