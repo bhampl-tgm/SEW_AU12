eaf7a63acfb16f0f02fbe410c3547b474827d988
tgm-patrick-koc <cash1v@hotmail.com>
2014-02-19 12:32:41 +0100

    nearly finished guiv2,working but no "wrong input test",no comments

--- src/geyerhamplkocsis/Gui.java
+++ src/geyerhamplkocsis/Gui.java
@@ -31,6 +31,10 @@ public class Gui extends JFrame {
 	JButton copy_to_clipboard2 = new JButton("Copy KeywordCipher Output to Clipboard");
 	JButton copy_to_clipboard3 = new JButton("Copy SubstitutionCipher Output to Clipboard");
 	JButton copy_to_clipboard4 = new JButton("Copy ShiftCipher Output to Clipboard");
+	JButton copy_output_to_input1 = new JButton("Copy MonoCipher Output to Input");
+	JButton copy_output_to_input2 = new JButton("Copy KeywordCipher Output to Input");
+	JButton copy_output_to_input3 = new JButton("Copy SubstitutionCipher Output to Input");
+	JButton copy_output_to_input4 = new JButton("Copy ShiftCipher Output to Input");
 	JComboBox<String> encryptmode = new JComboBox<String>();
 	String label_shiftci = "ShiftCipher";
 	String label_keywoci = "KeyWordCipher";
@@ -52,9 +56,10 @@ public class Gui extends JFrame {
 	public Gui(){
 		JTabbedPane tabbedPane = new JTabbedPane();
 		this.setTitle("EncryptDecrypt");
-		this.setSize(700, 500);
+		this.setSize(700, 300);
 		this.setLayout(new BorderLayout());
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
 		
 		JPanel input_panel_keyword_cipher= new JPanel();
 		JPanel input_panel_subst_cipher = new JPanel();
@@ -78,15 +83,20 @@ public class Gui extends JFrame {
 		copy_to_clipboard3.addActionListener(new ButtonListener());
 		copy_to_clipboard4.addActionListener(new ButtonListener());
 		
+		copy_output_to_input1.addActionListener(new ButtonListener());
+		copy_output_to_input2.addActionListener(new ButtonListener());
+		copy_output_to_input3.addActionListener(new ButtonListener());
+		copy_output_to_input4.addActionListener(new ButtonListener());
+		
 		output.setEditable(false);
 		output2.setEditable(false);
 		output3.setEditable(false);
-		output4.setEditable(false);
-
+		output4.setEditable(false);		
 		/*
 		 * 
 		 */
-		GridLayout input_layout = new GridLayout(4,2);
+		
+		GridLayout input_layout = new GridLayout(2,2);
 		input_layout.setVgap(10);
 		input_layout.setHgap(0);
 		input_panel_mono.setLayout(input_layout);
@@ -94,13 +104,20 @@ public class Gui extends JFrame {
 		input_panel_mono.add(input);
 		input_panel_mono.add(new JLabel("Output"));
 		input_panel_mono.add(output);
-		input_panel_mono.add(encrypt);
-		input_panel_mono.add(decrypt);
-		input_panel_mono.add(copy_to_clipboard1);
+		
+		JPanel buttons_mono = new JPanel();
+		buttons_mono.add(encrypt);
+		buttons_mono.add(decrypt);
+		buttons_mono.add(copy_to_clipboard1);
+		buttons_mono.add(copy_output_to_input1);
+		
+		JPanel panel_mono = new JPanel(new BorderLayout());
+		panel_mono.add(input_panel_mono,BorderLayout.NORTH);
+		panel_mono.add(buttons_mono,BorderLayout.CENTER);
 		/*
 		 * 
 		 */
-		GridLayout input_layout2 = new GridLayout(5,2);
+		GridLayout input_layout2 = new GridLayout(3,2);
 		input_layout2.setVgap(10);
 		input_layout2.setHgap(0);
 		input_panel_keyword_cipher.setLayout(input_layout2);
@@ -110,10 +127,16 @@ public class Gui extends JFrame {
 		input_panel_keyword_cipher.add(output2);
 		input_panel_keyword_cipher.add(new JLabel("KeyWord"));
 		input_panel_keyword_cipher.add(keyword);
-		input_panel_keyword_cipher.add(encrypt2);
-		input_panel_keyword_cipher.add(decrypt2);
-		input_panel_keyword_cipher.add(copy_to_clipboard2);
 		
+		JPanel buttons_keyword = new JPanel();
+		buttons_keyword.add(encrypt2);
+		buttons_keyword.add(decrypt2);
+		buttons_keyword.add(copy_to_clipboard2);
+		buttons_keyword.add(copy_output_to_input2);
+		
+		JPanel panel_keyword = new JPanel(new BorderLayout());
+		panel_keyword.add(input_panel_keyword_cipher,BorderLayout.NORTH);
+		panel_keyword.add(buttons_keyword,BorderLayout.CENTER);
 		/*
 		 * 
 		 */
@@ -125,11 +148,19 @@ public class Gui extends JFrame {
 		input_panel_subst_cipher.add(input3);
 		input_panel_subst_cipher.add(new JLabel("Output"));
 		input_panel_subst_cipher.add(output3);
-		input_panel_subst_cipher.add(new JLabel("Substitute Word"));
+		input_panel_subst_cipher.add(new JLabel("Alternative Alphabet(30 Letters)"));
 		input_panel_subst_cipher.add(subst_word);
-		input_panel_subst_cipher.add(encrypt3);
-		input_panel_subst_cipher.add(decrypt3);
-		input_panel_subst_cipher.add(copy_to_clipboard3);
+
+		
+		JPanel buttons_subst = new JPanel();
+		buttons_subst.add(encrypt3);
+		buttons_subst.add(decrypt3);
+		buttons_subst.add(copy_to_clipboard3);
+		buttons_subst.add(copy_output_to_input3);
+		
+		JPanel panel_subst = new JPanel(new BorderLayout());
+		panel_subst.add(input_panel_subst_cipher,BorderLayout.NORTH);
+		panel_subst.add(buttons_subst,BorderLayout.CENTER);
 		/*
 		 * 
 		 */
@@ -144,16 +175,23 @@ public class Gui extends JFrame {
 		input_panel_shift_cipher.add(output4);
 		input_panel_shift_cipher.add(new JLabel("Shift by Amount(only numbers)"));
 		input_panel_shift_cipher.add(shift_num);
-		input_panel_shift_cipher.add(encrypt4);
-		input_panel_shift_cipher.add(decrypt4);
-		input_panel_shift_cipher.add(copy_to_clipboard4);
+		
+		JPanel buttons_shift = new JPanel();
+		buttons_shift.add(encrypt4);
+		buttons_shift.add(decrypt4);
+		buttons_shift.add(copy_to_clipboard4);
+		buttons_shift.add(copy_output_to_input4);
+		
+		JPanel panel_shift = new JPanel(new BorderLayout());
+		panel_shift.add(input_panel_shift_cipher,BorderLayout.NORTH);
+		panel_shift.add(buttons_shift,BorderLayout.CENTER);
 		/*
 		 * 
 		 */
-		tabbedPane.addTab("Monoalphabetic Cipher", input_panel_mono);
-        tabbedPane.addTab("KeywordCipher", input_panel_keyword_cipher);
-        tabbedPane.addTab("SubstitutionCipher", input_panel_subst_cipher);
-        tabbedPane.addTab("ShiftCipher", input_panel_shift_cipher);
+		tabbedPane.addTab("Monoalphabetic Cipher", panel_mono);
+        tabbedPane.addTab("KeywordCipher", panel_keyword);
+        tabbedPane.addTab("SubstitutionCipher", panel_subst);
+        tabbedPane.addTab("ShiftCipher", panel_shift);
 		this.add(tabbedPane,BorderLayout.CENTER);
 		//this.add(buttons_panel,BorderLayout.SOUTH);
 		//this.add(combobox_panel,BorderLayout.NORTH);
@@ -186,6 +224,7 @@ public class Gui extends JFrame {
 			}
 			if(event.getText().equals("Encrypt SubstitutionCipher")){
 				String text_to_encrypt=input3.getText();
+					String alpha = "";
 	        	  SubstitutionCipher a = new SubstitutionCipher(subst_word.getText());
 	        	  String encrypted=a.encrypt(text_to_encrypt);
 	        	  output3.setText(encrypted);
@@ -197,60 +236,78 @@ public class Gui extends JFrame {
 		        	  output4.setText(encrypted);
 			}
 			
-			
+			/*
+			 * Das sind die 
+			 */
 			
 			if(event.getText().equals("Copy MonoCipher Output to Clipboard")){
-				StringSelection stringSelection = new StringSelection (copy_to_clipboard1.getText());
+				StringSelection stringSelection = new StringSelection (output.getText());
 				Clipboard clpbrd = Toolkit.getDefaultToolkit ().getSystemClipboard ();
 				clpbrd.setContents (stringSelection, null);
 			}
 			if(event.getText().equals("Copy KeywordCipher Output to Clipboard")){
-				StringSelection stringSelection = new StringSelection (copy_to_clipboard2.getText());
+				StringSelection stringSelection = new StringSelection (output2.getText());
 				Clipboard clpbrd = Toolkit.getDefaultToolkit ().getSystemClipboard ();
 				clpbrd.setContents (stringSelection, null);
 			}
 			if(event.getText().equals("Copy SubstitutionCipher Output to Clipboard")){
-				StringSelection stringSelection = new StringSelection (copy_to_clipboard3.getText());
+				StringSelection stringSelection = new StringSelection (output3.getText());
 				Clipboard clpbrd = Toolkit.getDefaultToolkit ().getSystemClipboard ();
 				clpbrd.setContents (stringSelection, null);
 			}
 			if(event.getText().equals("Copy ShiftCipher Output to Clipboard")){
-				StringSelection stringSelection = new StringSelection (copy_to_clipboard4.getText());
+				StringSelection stringSelection = new StringSelection (output4.getText());
 				Clipboard clpbrd = Toolkit.getDefaultToolkit ().getSystemClipboard ();
 				clpbrd.setContents (stringSelection, null);
 			}
+			/*
+			 *Das sind die Output to Input buttons
+			 */
+			if(event.getText().equals("Copy MonoCipher Output to Input")){
+				input.setText(output.getText());
+			}
+			if(event.getText().equals("Copy KeywordCipher Output to Input")){
+				input2.setText(output2.getText());
+			}
+			if(event.getText().equals("Copy SubstitutionCipher Output to Input")){
+				input3.setText(output3.getText());
+			}
+			if(event.getText().equals("Copy ShiftCipher Output to Input")){
+				input4.setText(output4.getText());
+			}
+			
 			
 			/*
 			 * Wenn der Button Decrypt gedr��ckt wird, wird mit dem gerade ausgew��hltem 
 			 * modus(String Wert in der JCombobox(=encryptmode)) 
 			 * der Text  (in der JTexArea(=input) angegeben) Entschl��sselt(decrypted) 
 			 */
-			if(event.getText().equals("Decrypt")){
-				if(selected_item.equals("ShiftCipher")){
-		        	  String text_to_decrypt=input.getText();
-		        	  ShiftCipher a = new ShiftCipher(2);
-		        	  String decrypted = a.decrypt(text_to_decrypt);
-		        	  output.setText(decrypted);
-		          }
-		          if(selected_item.equals("KeyWordCipher")){
-		        	  String text_to_decrypt=input.getText();
-		        	  KeywordCipher a = new KeywordCipher("test");
-		        	  String decrypted = a.decrypt(text_to_decrypt);
-		        	  output.setText(decrypted);
-		          }
-		          if(selected_item.equals("SubstituationCipher")){
-		        	  String text_to_decrypt=input.getText();
-		        	  SubstitutionCipher a = new SubstitutionCipher("test");
-		        	  String decrypted = a.decrypt(text_to_decrypt);
-		        	  output.setText(decrypted);
-		          }
-		          if(selected_item.equals("MonoAlphabeticCipher")){
-		        	  String text_to_decrypt=input.getText();
-		        	  MonoalphabeticCipher a = new MonoalphabeticCipher();
-		        	  String decrypted = a.decrypt(text_to_decrypt);
-		        	  output.setText(decrypted);
-		          }
+			
+			if(event.getText().equals("Decrypt MonoCipher")){
+				String text_to_decrypt=input.getText();
+	        	  MonoalphabeticCipher a = new MonoalphabeticCipher();
+	        	  String decrypted=a.decrypt(text_to_decrypt);
+	        	  output.setText(decrypted);
 			}
+			if(event.getText().equals("Decrypt KeywordCipher")){
+				String text_to_decrypt=input2.getText();
+	        	  KeywordCipher a = new KeywordCipher(keyword.getText());
+	        	  String decrypted=a.decrypt(text_to_decrypt);
+	        	  output2.setText(decrypted);
+			}
+			if(event.getText().equals("Decrypt SubstitutionCipher")){
+				String text_to_decrypt=input3.getText();
+	        	  SubstitutionCipher a = new SubstitutionCipher(subst_word.getText());
+	        	  String decrypted=a.decrypt(text_to_decrypt);
+	        	  output3.setText(decrypted);
+			}
+			if(event.getText().equals("Decrypt ShiftCipher")){
+		        	  String text_to_decrypt=input4.getText();
+		        	  ShiftCipher a = new ShiftCipher(Integer.parseInt(shift_num.getText()));
+		        	  String decrypted=a.decrypt(text_to_decrypt);
+		        	  output4.setText(decrypted);
+			}
+			
 			
 		}
 	}
